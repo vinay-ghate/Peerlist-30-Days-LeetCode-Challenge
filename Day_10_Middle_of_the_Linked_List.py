84a76108848d95febd32a9f386442fbaa8990954
Vinay Ghate <47355614+vinay-ghate@users.noreply.github.com>
2024-09-20 12:34:49 +0530

    Update Day_10_Middle_of_the_Linked_List.py

--- Day_10_Middle_of_the_Linked_List.py
+++ Day_10_Middle_of_the_Linked_List.py
@@ -3,6 +3,7 @@
 
 # Example: Input: head = [1,2,3,4,5] Output: [3,4,5]
 
+# Method 1
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cnt = 1
@@ -14,5 +15,14 @@ class Solution:
             cnt+=1
             if cnt%2==0:
                 temp = temp.next
+
+# Method 2
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        list = []
+        while head:
+            list.append(head)
+            head = head.next
+        return list[len(list)//2]
 
 # https://leetcode.com/problems/middle-of-the-linked-list/
